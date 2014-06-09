2c625d69dca091023812ae50d28bb4fe23f98c05
cl <cl>
2014-06-09 15:57:24 +0200

    hazlecast client dans aplli web

--- clients/web/src/main/java/cl/hazelcast/web/CacheController.java
+++ clients/web/src/main/java/cl/hazelcast/web/CacheController.java
@@ -1,52 +1,77 @@
 package cl.hazelcast.web;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hazelcast.core.IMap;
+
 @Controller
 public class CacheController {
 
-	@RequestMapping("/cache")
-	public String helloWorld(String cacheName, String id, String val, Model model) {
-		
-		List<String> cacheNames = new ArrayList<>();
-		cacheNames.add("");
-		cacheNames.add("map1");
-		cacheNames.add("map2");
-		cacheNames.add("map3");
-		cacheNames.add("map4");
-		cacheNames.add("map5");
-		cacheNames.add("map6");
+	@Resource(name = "map1")
+	private IMap<String, String> map1;
 
-		model.addAttribute("cacheNames",cacheNames);
-		
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("KEY1", "VALUE1");
-		map.put("KEY2", "VALUE2");		
-		
-		if(!(cacheName == null || cacheName.isEmpty())) {
-			
-			model.addAttribute("cacheName",cacheName);
+	@Resource(name = "map2")
+	private IMap<String, String> map2;
+
+	@Resource(name = "map3")
+	private IMap<String, String> map3;
+
+	@RequestMapping("/cache")
+	public String cache(String cacheName, String id, String val, Model model) {
+
+		IMap<String, String> map = null;
+
+		model.addAttribute("cacheNames", cacheNames);
+
+		if (!(cacheName == null || cacheName.isEmpty())) {
+
+			switch (cacheName) {
+			case "map1":
+				map = map1;
+				break;
+			case "map2":
+				map = map2;
+				break;
+			case "map3":
+				map = map3;
+				break;
+			default:
+				map = null;
+				break;
+			}
+
+			model.addAttribute("cacheName", cacheName);
 			model.addAttribute("cache", map);
-			
-			if(id != null && !id.isEmpty()) {
-				//modif de l'entrée
-				if(val != null && !val.isEmpty()) {
+
+			if (id != null && !id.isEmpty()) {
+				// modif de l'entrée
+				if (val != null && !val.isEmpty()) {
 					map.put(id, val);
-				}
-				else {
-					//suppression
+				} else {
+					// suppression
 					map.remove(id);
 				}
 			}
 		}
-		
+
 		return "cache";
 	}
+
+	private static List<String> cacheNames = new ArrayList<>();
+
+	static {
+		cacheNames.add("");
+		cacheNames.add("map1");
+		cacheNames.add("map2");
+		cacheNames.add("map3");
+
+	}
+
 }
--- servers/db/src/main/resources/script.sql
+++ servers/db/src/main/resources/script.sql
@@ -1,5 +1,9 @@
 DROP ALL OBJECTS;
 CREATE TABLE TABLE1(ID VARCHAR(255) PRIMARY KEY, VAL VARCHAR(255));
+CREATE TABLE TABLE2(ID VARCHAR(255) PRIMARY KEY, VAL VARCHAR(255));
+CREATE TABLE TABLE3(ID VARCHAR(255) PRIMARY KEY, VAL VARCHAR(255));
+
+
 
 insert into TABLE1 values('KEY1','VALUE1');
 insert into TABLE1 values('KEY2','VALUE2');
@@ -16,27 +20,68 @@ insert into TABLE1 values('KEY12','VALUE12');
 insert into TABLE1 values('KEY13','VALUE13');
 insert into TABLE1 values('KEY14','VALUE14');
 insert into TABLE1 values('KEY15','VALUE15');
-insert into TABLE1 values('KEY16','VALUE16');
-insert into TABLE1 values('KEY17','VALUE17');
-insert into TABLE1 values('KEY18','VALUE18');
-insert into TABLE1 values('KEY19','VALUE19');
-insert into TABLE1 values('KEY20','VALUE20');
-insert into TABLE1 values('KEY21','VALUE21');
-insert into TABLE1 values('KEY22','VALUE22');
-insert into TABLE1 values('KEY23','VALUE23');
-insert into TABLE1 values('KEY24','VALUE24');
-insert into TABLE1 values('KEY25','VALUE25');
-insert into TABLE1 values('KEY26','VALUE26');
-insert into TABLE1 values('KEY27','VALUE27');
-insert into TABLE1 values('KEY28','VALUE28');
-insert into TABLE1 values('KEY29','VALUE29');
-insert into TABLE1 values('KEY30','VALUE30');
-insert into TABLE1 values('KEY31','VALUE31');
-insert into TABLE1 values('KEY32','VALUE32');
-insert into TABLE1 values('KEY33','VALUE33');
-insert into TABLE1 values('KEY34','VALUE34');
-insert into TABLE1 values('KEY35','VALUE35');
-insert into TABLE1 values('KEY36','VALUE36');
-insert into TABLE1 values('KEY37','VALUE37');
-insert into TABLE1 values('KEY38','VALUE38');
-insert into TABLE1 values('KEY39','VALUE39');
+
+insert into TABLE2 values('KEY1','VALUE1');
+insert into TABLE2 values('KEY2','VALUE2');
+insert into TABLE2 values('KEY3','VALUE3');
+insert into TABLE2 values('KEY4','VALUE4');
+insert into TABLE2 values('KEY5','VALUE5');
+insert into TABLE2 values('KEY6','VALUE6');
+insert into TABLE2 values('KEY7','VALUE7');
+insert into TABLE2 values('KEY8','VALUE8');
+insert into TABLE2 values('KEY9','VALUE9');
+insert into TABLE2 values('KEY10','VALUE10');
+insert into TABLE2 values('KEY11','VALUE11');
+insert into TABLE2 values('KEY12','VALUE12');
+insert into TABLE2 values('KEY13','VALUE13');
+insert into TABLE2 values('KEY14','VALUE14');
+insert into TABLE2 values('KEY15','VALUE15');
+
+insert into TABLE3 values('KEY1','VALUE1');
+insert into TABLE3 values('KEY2','VALUE2');
+insert into TABLE3 values('KEY3','VALUE3');
+insert into TABLE3 values('KEY4','VALUE4');
+insert into TABLE3 values('KEY5','VALUE5');
+insert into TABLE3 values('KEY6','VALUE6');
+insert into TABLE3 values('KEY7','VALUE7');
+insert into TABLE3 values('KEY8','VALUE8');
+insert into TABLE3 values('KEY9','VALUE9');
+insert into TABLE3 values('KEY10','VALUE10');
+insert into TABLE3 values('KEY11','VALUE11');
+insert into TABLE3 values('KEY12','VALUE12');
+insert into TABLE3 values('KEY13','VALUE13');
+insert into TABLE3 values('KEY14','VALUE14');
+insert into TABLE3 values('KEY15','VALUE15');
+
+
+
+
+
+
+--insert into TABLE1 values('KEY16','VALUE16');
+--insert into TABLE1 values('KEY17','VALUE17');
+--insert into TABLE1 values('KEY18','VALUE18');
+--insert into TABLE1 values('KEY19','VALUE19');
+--insert into TABLE1 values('KEY20','VALUE20');
+--insert into TABLE1 values('KEY21','VALUE21');
+--insert into TABLE1 values('KEY22','VALUE22');
+--insert into TABLE1 values('KEY23','VALUE23');
+--insert into TABLE1 values('KEY24','VALUE24');
+--insert into TABLE1 values('KEY25','VALUE25');
+--insert into TABLE1 values('KEY26','VALUE26');
+--insert into TABLE1 values('KEY27','VALUE27');
+--insert into TABLE1 values('KEY28','VALUE28');
+--insert into TABLE1 values('KEY29','VALUE29');
+--insert into TABLE1 values('KEY30','VALUE30');
+--insert into TABLE1 values('KEY31','VALUE31');
+--insert into TABLE1 values('KEY32','VALUE32');
+--insert into TABLE1 values('KEY33','VALUE33');
+--insert into TABLE1 values('KEY34','VALUE34');
+--insert into TABLE1 values('KEY35','VALUE35');
+--insert into TABLE1 values('KEY36','VALUE36');
+--insert into TABLE1 values('KEY37','VALUE37');
+--insert into TABLE1 values('KEY38','VALUE38');
+--insert into TABLE1 values('KEY39','VALUE39');
+
+
+
